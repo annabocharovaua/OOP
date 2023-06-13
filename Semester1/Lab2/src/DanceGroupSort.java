b2df5c9847b94a8f86c7c5f0cf3a45acae0befa1
Anna Bocharova <120383629+annabocharovaua@users.noreply.github.com>
2023-06-13 16:25:07 +0300

    Delete Semester1 directory

--- Semester1/Lab2/src/DanceGroupSort.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-public class DanceGroupSort implements Comparator<DanceGroup> {
-    public int compare(DanceGroup a, DanceGroup b)
-    {
-        return a.number - b.number;
-    }
-}
--- Semester1/Problems/Problem7/CyclicBarrier/src/FinalTask.java
+++
@@ -1,6 +0,0 @@
-public class FinalTask implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("All tasks complete!");
-    }
-}
